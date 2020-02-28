5b3f0963e459a916362ab5c4638ebfebaa92e8f3
Erikas Svazas <r0769473@komen.cs.kotnet.kuleuven.be>
2020-02-28 13:00:07 +0100

    Working on a sparse solvers.

--- main.f90
+++ main.f90
@@ -1,15 +1,37 @@
-!MODULE sparse_system_solvers
-!	IMPLICIT NONE
-!	SAVE
+MODULE sparse_system_solvers
+	IMPLICIT NONE
+	SAVE
 
-!CONTAINS
-!	FUNCTION solver_v1(A, b, m) result x
-!		INTEGER, INTENT(in) 				:: m
-!		REAL, DIMENSION(m,m), INTENT(in)	:: A
-!		REAL, DIMENSION(m)					:: b
-!		REAL, DIMENSION(m)					:: x
-!	END FUNCTION solver_v1
-!END MODULE
+CONTAINS
+	SUBROUTINE solver_v1(A, b, m, x)
+		INTEGER, INTENT(in) 				:: m
+		REAL, DIMENSION(m,m), INTENT(in)	:: A
+		REAL, DIMENSION(m)					:: b
+		REAL, DIMENSION(m), INTENT(out)		:: x
+		PRINT *, "AHA"
+
+
+
+
+			REAL(selected_real_kind(6,37)), DIMENSION(:,:), INTENT(in)				:: A
+			REAL(selected_real_kind(6,37)), DIMENSION(SIZE(A,DIM=1), SIZE(A,DIM=2))	:: AA
+			REAL(selected_real_kind(6,37)), DIMENSION(:),  INTENT(out)				:: lambdar, lambdai
+			REAL(selected_real_kind(6,37)), DIMENSION(:,:), ALLOCATABLE				:: VLR
+		  	REAL(selected_real_kind(6,37)), DIMENSION(:), ALLOCATABLE				:: work
+			INTEGER info, l_work
+			AA = A
+			ALLOCATE(work(1))
+			CALL SGEEV('N','N',SIZE(A, DIM=2), AA, SIZE(A, DIM=2), lambdar, lambdai, VLR, 1, VLR, 1, work, -1, info)
+			l_work = work(1)
+			DEALLOCATE(work)
+			ALLOCATE(work(l_work))
+			CALL SGEEV('N','N',SIZE(A, DIM=2), AA, SIZE(A, DIM=2), lambdar, lambdai, VLR, 1, VLR, 1, work, l_work, info)
+			DEALLOCATE(work)			
+
+
+
+	END SUBROUTINE solver_v1
+END MODULE sparse_system_solvers
 
 SUBROUTINE get_sparse_matrix(A, m)
 	INTEGER, INTENT(in) 		:: m
@@ -73,6 +95,13 @@ SUBROUTINE get_symmetric_sparse_matrix(A, m)
 	END DO
 END SUBROUTINE get_symmetric_sparse_matrix
 
+SUBROUTINE get_random_vector(b, m)
+	INTEGER, INTENT(in) 	:: m
+	REAL, DIMENSION(m)		:: b
+
+	CALL RANDOM_NUMBER(b)
+END SUBROUTINE get_random_vector
+
 
 
 
@@ -80,14 +109,18 @@ END SUBROUTINE get_symmetric_sparse_matrix
 PROGRAM Main
 	!USE sparse_system_solvers
 
-	INTEGER, PARAMETER	::	m = 6		
-	REAL		A(m,m)
+	INTEGER, PARAMETER		:: m = 6		
+	REAL, DIMENSION(m,m)	:: A
+	REAL, DIMENSION(m)		:: b
 	INTEGER i
 
 	CALL get_sparse_matrix(A, m)
 	print *, A
 	print *, "------------"
 	CALL get_symmetric_sparse_matrix(A, m)
+
+	
+	CALL get_random_vector(b, m)
 
 	! COUT matrix to file
 	DO i=1,m
