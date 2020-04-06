a132cd373258115c961b19a6bbe57103cfa1a6ae
Erikas_Svazas <erikas.svazas@ff.stud.vu.lt>
2020-04-06 23:19:06 +0300

    Add Pardiso with CG iterates.

--- main.f90
+++ main.f90
@@ -17,10 +17,10 @@ CONTAINS
 	! LU factorization with partial pivoting
 	SUBROUTINE solver_v1(A, b, m, x)
 		INTEGER								  	:: m
-		REAL, DIMENSION(m,m), INTENT(in)		:: A
-		REAL, DIMENSION(m,m)					:: AA
-		REAL, DIMENSION(m), INTENT(in)			:: b
-		REAL, DIMENSION(m), INTENT(out)			:: x
+		REAL(8), DIMENSION(m,m), INTENT(in)		:: A
+		REAL(8), DIMENSION(m,m)					:: AA
+		REAL(8), DIMENSION(m), INTENT(in)		:: b
+		REAL(8), DIMENSION(m), INTENT(out)		:: x
 		INTEGER, DIMENSION(m)					:: IPIV
 		INTEGER info, l_work
 		
@@ -40,10 +40,10 @@ CONTAINS
 	! Cholesky factorization
 	SUBROUTINE solver_v2(A, b, m, x)
 		INTEGER								  	:: m
-		REAL, DIMENSION(m,m), INTENT(in)		:: A
-		REAL, DIMENSION(m,m)					:: AA, A_u
-		REAL, DIMENSION(m), INTENT(in)			:: b
-		REAL, DIMENSION(m), INTENT(out)			:: x
+		REAL(8), DIMENSION(m,m), INTENT(in)		:: A
+		REAL(8), DIMENSION(m,m)					:: AA, A_u
+		REAL(8), DIMENSION(m), INTENT(in)			:: b
+		REAL(8), DIMENSION(m), INTENT(out)			:: x
 		INTEGER, DIMENSION(m)					:: IPIV
 		INTEGER info, l_work
 		
@@ -64,10 +64,10 @@ CONTAINS
  	! and B are N-by-NRHS matrices.
 	SUBROUTINE solver_v3(A, b, m, x)
 		INTEGER								  	:: m, nsuper, info, i, j
-		REAL, DIMENSION(m,m), INTENT(in)		:: A
-		REAL, DIMENSION(m,m)					:: A_u
-		REAL, DIMENSION(CEILING(m/2.0),m)			:: A_banded
-		REAL, DIMENSION(m)						:: b, x
+		REAL(8), DIMENSION(m,m), INTENT(in)		:: A
+		REAL(8), DIMENSION(m,m)					:: A_u
+		REAL(8), DIMENSION(CEILING(m/2.0),m)			:: A_banded
+		REAL(8), DIMENSION(m)						:: b, x
 
 		x = b
 		nsuper = 1
@@ -93,11 +93,11 @@ END MODULE sparse_system_solvers
 ! DOES NOT WORK: IDEA - set work variable properly - allocate, deallocate
 SUBROUTINE get_sparse_matrix_llapack(A, m)
 	INTEGER, INTENT(in) 				:: m
-	REAL, DIMENSION(m,m)				:: A
+	REAL(8), DIMENSION(m,m)				:: A
 	INTEGER, DIMENSION(4)				:: iseed
-	REAL, DIMENSION(m)					:: diag, dl, dr, ipivot
+	REAL(8), DIMENSION(m)					:: diag, dl, dr, ipivot
 	INTEGER, DIMENSION(m)				:: iwork
-	REAL sparsity, anorm
+	REAL(8) sparsity, anorm
 	INTEGER info
 
 
@@ -115,10 +115,10 @@ END SUBROUTINE get_sparse_matrix_llapack
 
 SUBROUTINE get_sparse_matrix(A, m, rand_idx, rand_vect_real)
 	INTEGER, INTENT(in) 				:: m
-	REAL, DIMENSION(m,m)				:: A
-	REAL, DIMENSION(m), INTENT(IN)		:: rand_vect_real
-	REAL, DIMENSION(2*m), INTENT(IN)	:: rand_idx
-	REAL, DIMENSION(2*m)				:: rand_idx_COPY
+	REAL(8), DIMENSION(m,m)				:: A
+	REAL(8), DIMENSION(m), INTENT(IN)		:: rand_vect_real
+	REAL(8), DIMENSION(2*m), INTENT(IN)	:: rand_idx
+	REAL(8), DIMENSION(2*m)				:: rand_idx_COPY
 	INTEGER, DIMENSION(m)				:: idx_x, idx_y, rand_vect_int
 	INTEGER	i,j
 	
@@ -149,10 +149,10 @@ END SUBROUTINE get_sparse_matrix
 
 SUBROUTINE get_symmetric_sparse_matrix(A, m, rand_idx, rand_vect_real)
 	INTEGER, INTENT(in) 				:: m
-	REAL, DIMENSION(m,m)				:: A
-	REAL, DIMENSION(m)					:: rand_vect_real
-	REAL, DIMENSION(2*m), INTENT(IN)	:: rand_idx
-	REAL, DIMENSION(2*m)				:: rand_idx_COPY
+	REAL(8), DIMENSION(m,m)				:: A
+	REAL(8), DIMENSION(m)					:: rand_vect_real
+	REAL(8), DIMENSION(2*m), INTENT(IN)	:: rand_idx
+	REAL(8), DIMENSION(2*m)				:: rand_idx_COPY
 	INTEGER, DIMENSION(m)				:: idx_x, idx_y, rand_vect_int
 	INTEGER	i,j
 	
@@ -184,22 +184,48 @@ END SUBROUTINE get_symmetric_sparse_matrix
 
 SUBROUTINE get_random_vector(b, m)
 	INTEGER				 	:: m
-	REAL, DIMENSION(m)		:: bb, b
+	REAL(8), DIMENSION(m)		:: bb, b
 
 	CALL RANDOM_SEED()
 	CALL RANDOM_NUMBER(bb)
 	b = FLOOR((m)*bb)
 END SUBROUTINE get_random_vector
 
+!SUBROUTINE  sparse_to_CSR(ADNS, m, Acsr, AI, AJ)
+!    IMPLICIT NONE
+!    INTEGER                 :: m, info
+!    REAL(8), DIMENSION(m,m)    :: ADNS
+!    REAL(8), DIMENSION(m*m)    :: Acsr
+!    INTEGER, DIMENSION(m+1) :: AI
+!    INTEGER, DIMENSION(m*m) :: AJ
+
+!    integer job(8)
+!    info = 0
+!    job(1)=0
+!    job(2)= 1
+!    job(3)= 1
+!    job(4)= 2
+!    job(5)=m*m
+!    job(6)=1
+
+!    CALL mkl_ddnscsr(job,m,m,ADNS,m,Acsr,AJ,AI,info)
+!END SUBROUTINE sparse_to_CSR
+
 PROGRAM Main
 	USE sparse_system_solvers
 	USE eigv_module
+    USE cg_jacobian_solver
+    USE pardiso_sparse_solver
 
-	INTEGER, PARAMETER		:: m = 4	
-	REAL, DIMENSION(m,m)	:: A
-	REAL, DIMENSION(m)		:: b, x1, x2, x3, lambdar, lambdai
-	REAL, DIMENSION(m)		:: rand_vect_real
-	REAL, DIMENSION(2*m)	:: rand_idx
+	INTEGER, PARAMETER		    :: m = 4
+	REAL(8), DIMENSION(m,m)	    :: A
+	REAL(8), DIMENSION(m)		:: b, x1, x2, x3, lambdar, lambdai
+	REAL(8), DIMENSION(m)		:: rand_vect_real
+	REAL(8), DIMENSION(2*m)	    :: rand_idx
+
+    !REAL(8), DIMENSION(m*m)  :: Acsr
+    !INTEGER, DIMENSION(m+1) :: AI
+    !INTEGER, DIMENSION(m*m) :: AJ
 	INTEGER i,n
 	!REAL start, finish
 
@@ -212,18 +238,28 @@ PROGRAM Main
 
 	!CALL get_sparse_matrix_llapack(A,m)
 	!CALL get_sparse_matrix(A, m, rand_idx, rand_vect_real)
-	CALL get_symmetric_sparse_matrix(A, m, rand_idx, rand_vect_real)
+	!CALL get_symmetric_sparse_matrix(A, m, rand_idx, rand_vect_real)
 	!CALL get_random_vector(b, m)
 
 	! EXAMPLE POSITIVE DEFINITE MATRIX:
 	!A = reshape((/2, -2, -3, -2, 5, 4, -3, 4, 5/), (/3,3/))
 	! POSITIVE DEFINITE AND SYMMETRIC!
 	A = reshape((/2, 1, 0, 0, 1, 3, 2, 0, 0, 2, 5, 2, 0, 0, 2, 3/), (/4,4/))
-	!b = (/7,-12,-12/)
 	b = (/21, 69, 34, 22/)
-	PRINT *, '---------+++---+++-----------'
-	CALL solver_v3(A, b, m, x3)
-	PRINT *, '---------+++---+++-----------'
+
+    !A = RESHAPE((/7,0,1,0,0,2,7,0,0,-4,8,0,2,0,0,0,0,0,1,0,0,0,0,5,0,0,0,7,0,0,9,0,0,0,0,0,5,1,5,0,0,0,0,0,0,-1,0,5,0,0,0,0,0,0,11,0,0,0,0,0,0,0,0,5/), (/8,8/))
+    !b = (/21, 69, 34, 22, 12, 13, 16, 18/)
+	!PRINT *, '---------+++---+++-----------'
+	!CALL solver_v1(A, b, m, x1)
+	!PRINT *, '---------+++ V1---+++-----------'
+    PRINT *, '---------+++ PARDISO SOLVER ---+++-----------'
+    CALL pardiso_sym_solver(A, b, m, x1)
+    PRINT *, x1
+    PRINT *, '---------+++ CG JACOBIAN ---+++-----------'
+    CALL cg_solver(A, b, m, x2)
+    PRINT *, x2
+
+    !CALL sparse_to_CSR(A, m, Acsr, AI, AJ)
 
 	!CALL CPU_TIME(start)	
 	!CALL solver_v1(A, b, m, x1)
@@ -245,17 +281,17 @@ PROGRAM Main
 	!PRINT *, 'CHOLESKY SOLUTION: ', x2
 
 	! COUT matrix to file
-	!DO i=1,m
-	!	WRITE(12,*) A(i,:)
-	!END DO
+	DO i=1,m
+		WRITE(12,*) A(i,:)
+	END DO
 
-	!DO i=1,m
-	!	WRITE(11,*) b(i)
-	!END DO
+	DO i=1,m
+		WRITE(11,*) b(i)
+	END DO
 
-	!DO i=1,m
-	!	WRITE(10,*) x1(i)
-	!END DO
+	DO i=1,m
+		WRITE(10,*) x1(i)
+	END DO
 
 	!DO i=1,m
 	!	WRITE(9,*) x2(i)
---
+++ pardiso_sparse_solver.f90
@@ -0,0 +1,160 @@
+! INSTRUCTIONS TO COMPILE AND LINK ON MAC:
+! ifort -i8 -c eigv_module.f90 cg_jacobian_solver.f90 pardiso_sparse_solver.f90 main.f90 -mkl
+! ifort -o main eigv_module.o cg_jacobian_solver.o pardiso_sparse_solver.o main.o  -L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
+!----------------------------------------------------------------------
+! Based on "pardiso_sym_f90.f90" example program to show the use of the "PARDISO" routine
+! for symmetric linear systems
+!---------------------------------------------------------------------
+MODULE pardiso_sparse_solver
+    USE mkl_pardiso
+    !INCLUDE 'mkl_pardiso.fi'
+    INTEGER, PARAMETER :: dp = KIND(1.0D0)
+CONTAINS
+    SUBROUTINE  uncompressed_to_CSR_converter(ADNS, m, ja, ia, Acsr)
+    ! Convert a sparse matrix in uncompressed representation to the CSR format
+    ! Double precision
+        IMPLICIT NONE
+        INTEGER                 :: m, info
+        REAL(8), DIMENSION(m,m) :: ADNS
+        REAL(8), DIMENSION(m*m) :: Acsr
+        INTEGER, DIMENSION(m+1) :: ia
+        INTEGER, DIMENSION(m*m) :: ja
+        INTEGER, DIMENSION(8)   :: job
+
+        info = 0
+        job(1)=0
+        job(2)= 1
+        job(3)= 1
+        job(4)= 1
+        job(5)=m*m
+        job(6)=1
+
+        Acsr = 0
+        ja = 0
+        ia = 0
+
+        CALL mkl_ddnscsr(job,m,m,ADNS,m,Acsr,ja,ia,info)
+    END SUBROUTINE uncompressed_to_CSR_converter
+    
+    SUBROUTINE pardiso_sym_solver(A, b, m, x)
+        INTEGER                                         :: m, zero_index
+        REAL(KIND=DP), DIMENSION(m,m), INTENT(inout)    :: A
+        REAL(KIND=DP), DIMENSION(m)                     :: b
+        REAL(KIND=DP), DIMENSION(m)                     :: x
+        !.. Internal solver memory pointer
+        TYPE(MKL_PARDISO_HANDLE), ALLOCATABLE           :: pt(:)
+        !.. All other variables
+        INTEGER, ALLOCATABLE                            :: iparm(:)
+        REAL(KIND=DP), DIMENSION(m*m)                   :: acsr
+        INTEGER, DIMENSION(m+1)                         :: ia
+        INTEGER, DIMENSION(m*m)                         :: ja
+        INTEGER, DIMENSION(:), ALLOCATABLE              :: JAA
+        REAL(KIND=DP), DIMENSION(:), ALLOCATABLE        :: acsrr
+        REAL(KIND=DP) ddum(1)
+        INTEGER maxfct, mnum, mtype, phase, nrhs, error, msglvl
+        INTEGER error1
+        INTEGER i, idum(1)
+
+        !.. Fill all arrays containing matrix data.
+        nrhs = 1
+        maxfct = 1
+        mnum = 1
+
+        ! Convert a sparse matrix in uncompressed representation to the CSR format
+        CALL uncompressed_to_CSR_converter(A, m, ja, ia, acsr)
+
+        zero_index = ia(size(ia)) - 1
+        ALLOCATE(jaa(zero_index))
+        ALLOCATE(acsrr(zero_index))
+        jaa = ja(:zero_index)
+        acsrr = acsr(:zero_index)
+
+        !..
+        !.. Set up PARDISO control parameter
+        !..
+        ALLOCATE(iparm(64))
+
+        iparm = 0
+        iparm(1) = 1 ! no solver default
+        iparm(2) = 2 ! fill-in reordering from METIS
+        iparm(4) = 0 ! no iterative-direct algorithm
+        iparm(5) = 0 ! no user fill-in reducing permutation
+        iparm(6) = 0 ! =0 solution on the first n components of x
+        iparm(8) = 2 ! numbers of iterative refinement steps
+        iparm(10) = 13 ! perturb the pivot elements with 1E-13
+        iparm(11) = 1 ! use nonsymmetric permutation and scaling MPS
+        iparm(13) = 0 ! maximum weighted matching algorithm is switched-off (default for symmetric). Try iparm(13) = 1 in case of inappropriate accuracy
+        iparm(14) = -1 ! Output: number of perturbed pivots
+        iparm(17) = -1 ! Output: number of non-zero elements in the factors
+        iparm(18) = -1 ! Output: number of nonzeros in the factor LU
+        iparm(19) = -1 ! Output: Mflops for LU factorization
+        iparm(20) = -1 ! Output: Numbers of CG Iterations
+        iparm(36) = 0 ! matrix stored in CSR format
+        
+
+        error  = 0 ! initialize error flag
+        msglvl = 0 ! print statistical information
+        mtype  = 2 ! symmetric, positive definite
+
+        !.. Initialize the internal solver memory pointer. This is only
+        ! necessary for the FIRST call of the PARDISO solver.
+
+        ALLOCATE (pt(64))
+        DO i = 1, 64
+           pt(i)%DUMMY =  0
+        END DO
+
+        !.. Reordering and Symbolic Factorization, This step also allocates
+        ! all memory that is necessary for the factorization
+
+        phase = 11 ! only reordering and symbolic factorization
+        CALL pardiso(pt, maxfct, mnum, mtype, phase, m, acsrr, ia, jaa, &
+                      idum, nrhs, iparm, msglvl, ddum, ddum, error)
+        
+        !WRITE(*,*) 'Reordering completed ... '
+        IF (error /= 0) THEN
+           WRITE(*,*) 'The following ERROR was detected: ', error
+           GOTO 1000
+        END IF
+        !WRITE(*,*) 'Number of nonzeros in factors = ',iparm(18)
+        !WRITE(*,*) 'Number of factorization MFLOPS = ',iparm(19)
+
+        !.. Factorization.
+        phase = 22 ! only factorization
+        CALL pardiso(pt, maxfct, mnum, mtype, phase, m, acsrr, ia, jaa, &
+                      idum, nrhs, iparm, msglvl, ddum, ddum, error)
+        !WRITE(*,*) 'Factorization completed ... '
+        IF (error /= 0) THEN
+           WRITE(*,*) 'The following ERROR was detected: ', error
+           GOTO 1000
+        ENDIF
+
+        !.. Back substitution and iterative refinement
+        iparm(8) = 2 ! max numbers of iterative refinement steps
+        phase = 33 ! only solving
+        CALL pardiso(pt, maxfct, mnum, mtype, phase, m, acsrr, ia, jaa, &
+                      idum, nrhs, iparm, msglvl, b, x, error)
+        !WRITE(*,*) 'Solve completed ... '
+        IF (error /= 0) THEN
+           WRITE(*,*) 'The following ERROR was detected: ', error
+           GOTO 1000
+        ENDIF
+          
+        1000 CONTINUE
+        !.. Termination and release of memory
+        phase = -1 ! release internal memory
+        CALL pardiso(pt, maxfct, mnum, mtype, phase, m, ddum, idum, idum, &
+                      idum, nrhs, iparm, msglvl, ddum, ddum, error1)
+
+        IF (ALLOCATED(jaa)) DEALLOCATE(jaa)
+        IF (ALLOCATED(acsrr)) DEALLOCATE(acsrr)
+        IF (ALLOCATED(iparm)) DEALLOCATE(iparm)
+
+        IF (error1 /= 0) THEN
+           WRITE(*,*) 'The following ERROR on release stage was detected: ', error1
+           STOP 1
+        ENDIF
+
+        IF (error /= 0) STOP 1
+    END SUBROUTINE pardiso_sym_solver
+END MODULE pardiso_sparse_solver
