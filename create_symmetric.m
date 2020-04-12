384fb5e6bc1994395419aef11f149434a5f30486
Erikas_Svazas <erikas.svazas@ff.stud.vu.lt>
2020-04-12 13:25:36 +0300

    Fix matlab visualization file

--- create_symmetric.m
+++ create_symmetric.m
@@ -10,9 +10,6 @@ figure
 fortran=  reshape(optimization_result,[sqrt(n_design_cells)/2,sqrt(n_design_cells)/2])';
 
 fortran = [(fortran),fliplr(fortran);flipud(fortran), fliplr(flipud(fortran))];
-
-x=linspace(1,10,50);
-y=linspace(1,10,50);
 heatmap(fortran,'gridvisible','off')
 title('optimal solution')
 
